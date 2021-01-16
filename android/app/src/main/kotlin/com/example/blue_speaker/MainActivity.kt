6c0152fc63e66edc5804ecb1f9c3bc55caddab7f
EmIkram <iqramulhaq00@gmail.com>
2021-01-16 16:39:23 +0500

    First commit

---
+++ android/app/src/main/kotlin/com/example/blue_speaker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blue_speaker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Color connectivity_color = Colors.white;
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+            child: Center(
+              child: CircleAvatar(
+                radius: 80.0,
+                child: GestureDetector(
+                  child: CircleAvatar(
+                      radius: 73.0,
+                      backgroundColor: Colors.black,
+                      child: Icon(Icons.bluetooth,size: 50,color:connectivity_color,)
+                  ),
+
+                  onTap: (){
+                    // Start scanning
+                    flutterBlue.startScan(timeout: Duration(seconds: 10));
+                    var subscription = flutterBlue.scanResults.listen((results) {
+                      // do something with scan results
+                      for (ScanResult r in results) {
+                        print('${r.device.name} found! rssi: ${r.rssi}');
+                      }
+                    });
+                    flutterBlue.stopScan();
+                  },
+
+
+                ),
+              ),
+            )
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
